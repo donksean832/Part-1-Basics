b4bc25f36a97ce5af441eccbee8f5c25a2833db1
Sean Donker <DonkSean832@GoTVDSB.ca>
2020-02-06 12:43:23 -0500

    added comments

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string favMovie; 
+
+            Console.Write(" SSS ");
+            Console.WriteLine(" DDDD ");
+            Console.Write("S   S");
+            Console.WriteLine(" D   D");
+            Console.Write("S    ");
+            Console.WriteLine(" D   D");
+            Console.Write(" SSS ");
+            Console.WriteLine(" D   D");
+            Console.Write("    S");
+            Console.WriteLine(" D   D");
+            Console.Write("S   S");
+            Console.WriteLine(" D   D");
+            Console.Write(" SSS ");
+            Console.WriteLine(" DDDD ");
+
+            firstName = ("Sean");
+            favMovie = ("Iron Man");
+
+
+
+            Console.ReadLine();        }
+    }
+}
